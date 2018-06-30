49db7b5ccba293336fa87d2c8952385ab9d271b3
dudueasy <dudueasy@126.com>
2018-06-30 23:26:13 +0800

    rearrange LeanCloud initializer

---
+++ src/js/initializers/av.js
@@ -0,0 +1,10 @@
+{
+  // init LeanCloud API
+  let APP_ID = '5aFB8rFzI89casQfQlXMYyUD-gzGzoHsz';
+  let APP_KEY = 'soVeyWExsNeEF7wYavb0epvP';
+  AV.init({
+    appId: APP_ID,
+    appKey: APP_KEY
+  });
+}
+
