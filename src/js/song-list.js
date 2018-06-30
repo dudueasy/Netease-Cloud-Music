292c82f52afc79b1eaac5c9d8fcfaf093477093d
dudueasy <dudueasy@126.com>
2018-07-01 05:46:10 +0800

    add click-active feature to songname list items

--- src/js/song-list.js
+++ src/js/song-list.js
@@ -21,6 +21,11 @@
       })
 
     },
+    activeItem(li){
+      let $li = $(li)
+      $li.addClass('active').
+        siblings('.active').removeClass('active')
+    },
     clearActive(){
       $(this.el).find('.active').removeClass('active')
     }
@@ -50,11 +55,24 @@
       this.view = view
       this.model = model
       this.view.render(this.model.data)
+      this.bindEventHub()
+      this.bindEvents()
+      this.getAllSongs()
 
-      this.model.find().then(()=>{
+    },
+    getAllSongs(){
+      return this.model.find().then(()=>{
         this.view.render(this.model.data)
       })
+    }
+    ,
+    bindEvents(){
+      $(this.view.el).on('click', 'li', (e)=>{
+      this.view.activeItem(e.currentTarget)
+      })
 
+    },
+    bindEventHub(){
       window.eventHub.on('upload', ()=>{
         this.view.clearActive()
       })
